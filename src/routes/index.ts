4d17df516cf8292f3977e13986afc0dccfb524af
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2024-09-26 14:24:48 -0400

    recharge wallet route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { registroCliente } from '../services'
+import { recargaBilletera, registroCliente } from '../services'
 
 const router = Router()
 
 router.post('/', (req, res) => registroCliente(req, res))
+
+router.put('/', (req, res) => recargaBilletera(req, res))
 
 export default router
