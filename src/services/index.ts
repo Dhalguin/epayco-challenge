8b235bcb55aefba0cee90ca275bc85454bd2c6c6
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2024-09-26 14:24:57 -0400

    recharge wallet service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { RegistrarClienteDTO } from '../schemas/client.dto'
+import { RecargaBilleteraDTO, RegistrarClienteDTO } from '../schemas/client.dto'
 import { ClientModel } from '../schemas'
 
 export const registroCliente = async (req: Request<{}, {}, RegistrarClienteDTO>, res: Response) => {
@@ -20,6 +20,37 @@ export const registroCliente = async (req: Request<{}, {}, RegistrarClienteDTO>,
       success: 'OK',
       data: clientCreated,
       message: 'Cliente registrado correctamente',
+    })
+  } catch (err) {
+    return res.status(500).json({
+      sucess: 'FAILED',
+      message: 'Error del servidor',
+    })
+  }
+}
+
+export const recargaBilletera = async (req: Request<{}, {}, RecargaBilleteraDTO>, res: Response) => {
+  try {
+    const { documento, celular, valor } = req.body
+
+    if (valor <= 0)
+      return res.status(400).json({
+        success: 'FAILED',
+        message: 'Ingrese un monto válido',
+      })
+
+    const client = await ClientModel.findOne({ $and: [{ documento }, { celular }] })
+    if (!client)
+      return res.status(401).json({
+        success: 'FAILED',
+        message: 'No se pudo recargar la billetera',
+      })
+
+    await client.updateOne({ valor: client.valor + valor })
+
+    return res.status(200).json({
+      success: 'OK',
+      message: 'Ha recargado su billetera correctamente',
     })
   } catch (err) {
     return res.status(500).json({
