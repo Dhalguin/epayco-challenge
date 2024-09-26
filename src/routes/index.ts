d3d78e6d2c57135cd483958318f2235d27631f46
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2024-09-26 14:26:02 -0400

    check balance route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
-import { Router } from 'express'
-import { recargaBilletera, registroCliente } from '../services'
+import { Request, Router } from 'express'
+import { consultaSaldo, recargaBilletera, registroCliente } from '../services'
 
 const router = Router()
 
 router.post('/', (req, res) => registroCliente(req, res))
 
 router.put('/', (req, res) => recargaBilletera(req, res))
+
+router.get('/:documento', (req: Request<{ documento: string }, {}, {}, { celular: string }>, res) =>
+  consultaSaldo(req, res)
+)
 
 export default router
