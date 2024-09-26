5038b7757f83b6762484007a32efed618dfce2e4
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2024-09-26 13:52:11 -0400

    return function responses

--- src/services/index.ts
+++ src/services/index.ts
@@ -7,13 +7,24 @@ export const registroCliente = async (req: Request<{}, {}, RegistrarClienteDTO>,
     const { documento, email, celular } = req.body
 
     const exists = await ClientModel.findOne({ $or: [{ documento }, { email }, { celular }] })
-    if (exists) res.status(400).json({ success: 'FAILED', message: 'El cliente ya se encuentra registrado' })
+    if (exists)
+      return res.status(400).json({
+        success: 'FAILED',
+        message: 'El cliente ya se encuentra registrado',
+      })
 
     const clientCreated = await ClientModel.create(req.body)
     await clientCreated.save()
 
-    res.status(201).json({ success: 'OK', data: clientCreated, message: 'Cliente registrado correctamente' })
+    return res.status(201).json({
+      success: 'OK',
+      data: clientCreated,
+      message: 'Cliente registrado correctamente',
+    })
   } catch (err) {
-    res.status(500).json({ sucess: 'FAILED', message: 'Error del servidor' })
+    return res.status(500).json({
+      sucess: 'FAILED',
+      message: 'Error del servidor',
+    })
   }
 }
